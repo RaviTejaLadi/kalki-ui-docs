114c843ed2e5599b48e116cf839ee479b0485f8a
Ravi Teja <ladi.ravi884781@gmail.com>
2024-12-18 03:37:11 +0530

    fix: add missing React import in categorizedRoutesComponents.tsx

--- src/components/Layout/ComponentsLayout/categorizedRoutesComponents.tsx
+++ src/components/Layout/ComponentsLayout/categorizedRoutesComponents.tsx
@@ -52,6 +52,7 @@ import {
   DocumentCheckIcon,
   DocumentMagnifyingGlassIcon,
 } from '@heroicons/react/24/outline';
+import React from 'react';
 
 interface CategoryRoutesComponentList {
   label: string;
