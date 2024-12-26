489b94b8864757472e5010be0698909f34969b21
RaviTeja884781 <ladi.ravi884781@gmail.com>
2024-12-26 18:17:46 +0530

    style: adjust margin and height for improved layout consistency in ComponentsOverviewPage

--- src/pages/componentsPage/ComponentsOverviewPage.tsx
+++ src/pages/componentsPage/ComponentsOverviewPage.tsx
@@ -18,13 +18,13 @@ const ComponentsOverviewPage: React.FC = () => {
       <Separator />
       <div className="container my-3">
         {categorizedRoutesComponents.map(({ category, components, id }) => (
-          <div className="mb-5">
+          <div className="mb-10">
             <h2 className="text-foreground font-semibold">{category}</h2>
             <Separator className="h-[0.5px] my-2" />
             <div key={id} id={id} className="mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
               {components.map(({ path, label, description, Icon }, idx) => (
                 <Link key={idx} to={path}>
-                  <Card className="border border-gray-100 mb-5 rounded-md hover:shadow-md transition dark:border-gray-200/10 dark:bg-background/10">
+                  <Card className="border h-full border-gray-100 mb-5 rounded-md hover:shadow-md transition dark:border-gray-200/10 dark:bg-background/10">
                     <CardHeader>
                       <div className="flex gap-3 items-center align-middle">
                         {Icon ? <Icon className="size-5 text-[var(--icon-color)] border p-1 rounded bg-background dark:border-gray-200/10 dark:bg-background/10" /> : null}
