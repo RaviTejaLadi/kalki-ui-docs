c054c0ca2c1382cf7ee97ebf3c5a27749799b584
RaviTeja884781 <ladi.ravi884781@gmail.com>
2024-12-27 11:34:37 +0530

    feat: refactor Avatar component to improve context handling and export additional types

--- src/components/common/Avatar/index.tsx
+++ src/components/common/Avatar/index.tsx
@@ -1,34 +1,28 @@
 import React, { useState } from 'react';
 import { cn } from '@/utils';
-import { AvatarProps } from './types';
 import { AvatarImage } from './AvatarImage';
 import { AvatarFallback } from './AvatarFallback';
 import { AvatarContext } from './context/AvatarContext';
+import type { AvatarContextType, AvatarProps, AvatarImageProps, AvatarFallbackProps } from './types';
 
-const Avatar = React.forwardRef<HTMLDivElement, AvatarProps>(
-  ({ children, className, size, shape, ...props }, ref) => {
-    const [hasError, setHasError] = useState(false);
+const Avatar = React.forwardRef<HTMLDivElement, AvatarProps>(({ children, className, size, shape, ...props }, ref) => {
+  const [hasError, setHasError] = useState(false);
 
-    return (
-      <AvatarContext.Provider 
-        value={{ 
-          size, 
-          shape, 
-          hasError,
-          onError: () => setHasError(true)
-        }}
-      >
-        <div
-          ref={ref}
-          className={cn('relative inline-flex', className)}
-          {...props}
-        >
-          {children}
-        </div>
-      </AvatarContext.Provider>
-    );
-  }
-);
+  return (
+    <AvatarContext.Provider
+      value={{
+        size,
+        shape,
+        hasError,
+        onError: () => setHasError(true),
+      }}
+    >
+      <div ref={ref} className={cn('relative inline-flex', className)} {...props}>
+        {children}
+      </div>
+    </AvatarContext.Provider>
+  );
+});
 
 Avatar.displayName = 'Avatar';
 
@@ -37,4 +31,4 @@ export default Object.assign(Avatar as React.FC<AvatarProps>, {
   Fallback: AvatarFallback,
 });
 
-export { AvatarFallback, AvatarImage };
+export { AvatarFallback, AvatarImage, AvatarContextType, AvatarProps, AvatarImageProps, AvatarFallbackProps };
