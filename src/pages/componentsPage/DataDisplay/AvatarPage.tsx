cd877ac3d6bcc04493798c0bb33162cdfae3f28f
RaviTeja884781 <ladi.ravi884781@gmail.com>
2025-02-19 11:06:48 +0530

    refactor: enhance AvatarPage layout and documentation rendering

--- src/pages/componentsPage/DataDisplay/AvatarPage.tsx
+++ src/pages/componentsPage/DataDisplay/AvatarPage.tsx
@@ -1,5 +1,26 @@
+import { SectionHeader } from '@/components/common/SectionHeader/SectionHeader';
+import Separator from '@/components/common/Separator/Separator';
+import TableList from '@/components/common/Table/TableList';
+import { docs, columns, rows } from './data/avatarDocs';
+import DocumentationRenderer from '@/components/shared/DocumentationRenderer/DocumentationRenderer';
+
 const AvatarPage = () => {
-  return <div>AvatarPage</div>;
+  return (
+    <div className="container">
+      <SectionHeader variant="transparent" size="sm">
+        <SectionHeader.Title className="tracking-wide">Avatar</SectionHeader.Title>
+        <SectionHeader.SubTitle className="tracking-wide">
+          Avatar represents people using icons, labels and images.
+        </SectionHeader.SubTitle>
+      </SectionHeader>
+      <Separator />
+      <DocumentationRenderer className="my-4" data={docs} />
+      <SectionHeader variant="transparent" size="sm">
+        <SectionHeader.Title className="tracking-wide">Props</SectionHeader.Title>
+      </SectionHeader>
+      <TableList columns={columns} rows={rows} code CodeColumn={0} />
+    </div>
+  );
 };
 
 export default AvatarPage;
