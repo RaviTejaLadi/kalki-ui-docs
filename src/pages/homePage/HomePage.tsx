e2fd563763554b37da767e6ed7225c8db5dc2ccb
RaviTeja884781 <ladi.ravi884781@gmail.com>
2025-02-19 17:06:54 +0530

    refactor: remove Spinner component from HomePage

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -4,12 +4,10 @@ import { FeaturesSection } from './FeaturesSection';
 import ComponentCategoriesSection from './ComponentCategoriesSection';
 import Div from '@/components/common/Div';
 import Head from '@/components/Head';
-import Spinner from '@/components/common/Spinner/Spinner';
 
 export default function Home() {
   return (
     <Div>
-      <Spinner />
       <Head title="Home" />
       <HeroSection />
       <FeaturesSection />
