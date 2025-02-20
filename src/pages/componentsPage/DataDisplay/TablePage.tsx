fb51f5c49fe1c32b9dea99899d393aef2687cd1d
RaviTeja884781 <ladi.ravi884781@gmail.com>
2025-02-20 11:31:52 +0530

    feat: add TablePage component with documentation and structured data display

--- src/pages/componentsPage/DataDisplay/TablePage.tsx
+++ src/pages/componentsPage/DataDisplay/TablePage.tsx
@@ -1,5 +1,78 @@
+import { SectionHeader } from '@/components/common/SectionHeader/SectionHeader';
+import Separator from '@/components/common/Separator';
+import TableList from '@/components/common/Table/TableList';
+import {
+  docs,
+  columns,
+  tableRows,
+  tableHeadRows,
+  tableBodyRows,
+  tableFooterRows,
+  tableRowRows,
+  tableCellRows,
+} from './data/tableDocs';
+import DocumentationRenderer from '@/components/shared/DocumentationRenderer/DocumentationRenderer';
+import { Heading } from '@/components/common/Typography';
+import Div from '@/components/common/Div';
+
 const TablePage = () => {
-  return <div>TablePage</div>;
+  return (
+    <div className="container">
+      <SectionHeader variant="transparent" size="sm">
+        <SectionHeader.Title className="tracking-wide">Tables</SectionHeader.Title>
+        <SectionHeader.SubTitle className="tracking-wide">
+          Table is used to display data in a structured, grid-like format consisting of rows and columns.
+        </SectionHeader.SubTitle>
+        <SectionHeader.SubTitle className="tracking-wide">
+          Tables are ideal for presenting data that is best understood in a row-column format, such as statistics,
+          schedules, comparisons, financial information, and any data that needs to be organized clearly.
+        </SectionHeader.SubTitle>
+      </SectionHeader>
+      <Separator />
+      <DocumentationRenderer className="my-4" data={docs} />
+      <SectionHeader variant="transparent" size="sm">
+        <SectionHeader.Title className="tracking-wide">Props</SectionHeader.Title>
+      </SectionHeader>
+      <Div className="space-y-5">
+        <Div>
+          <Heading size="h6" className="mb-2 text-muted-foreground">
+            Table
+          </Heading>
+          <TableList columns={columns} rows={tableRows} code CodeColumn={0} />
+        </Div>
+        <Div>
+          <Heading size="h6" className="mb-2 text-muted-foreground">
+            Table Header
+          </Heading>
+          <TableList columns={columns} rows={tableHeadRows} code CodeColumn={0} />
+        </Div>
+        <Div>
+          <Heading size="h6" className="mb-2 text-muted-foreground">
+            Table Body
+          </Heading>
+          <TableList columns={columns} rows={tableBodyRows} code CodeColumn={0} />
+        </Div>
+        <Div>
+          <Heading size="h6" className="mb-2 text-muted-foreground">
+            Table Footer
+          </Heading>
+          <TableList columns={columns} rows={tableFooterRows} code CodeColumn={0} />
+        </Div>
+        <Div>
+          <Heading size="h6" className="mb-2 text-muted-foreground">
+            Table Row
+          </Heading>
+          <TableList columns={columns} rows={tableRowRows} code CodeColumn={0} />
+        </Div>
+        <Div>
+          <Heading size="h6" className="mb-2 text-muted-foreground">
+            Table Cell
+          </Heading>
+          <TableList columns={columns} rows={tableCellRows} code CodeColumn={0} />
+        </Div>
+      </Div>
+    </div>
+  );
 };
 
 export default TablePage;
---
+++ src/pages/componentsPage/DataDisplay/data/tableDocs.tsx
@@ -0,0 +1,101 @@
+import Table from '@/components/common/Table';
+import { docsData } from '@/types/docsData';
+
+export const docs: docsData[] = [
+  {
+    title: 'Basic Usage',
+    desc: 'A simple Table component with default styling.',
+    code: `
+import React from 'react';
+import { Table } from './Table';
+
+const BasicExample = () => {
+return (
+    <Table>
+        <Table.Head>
+            <Table.Row>
+            <Table.Cell isHeader>Header 1</Table.Cell>
+            <Table.Cell isHeader>Header 2</Table.Cell>
+            </Table.Row>
+        </Table.Head>
+        <Table.Body>
+            <Table.Row>
+            <Table.Cell>Row 1, Cell 1</Table.Cell>
+            <Table.Cell>Row 1, Cell 2</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+            <Table.Cell>Row 2, Cell 1</Table.Cell>
+            <Table.Cell>Row 2, Cell 2</Table.Cell>
+            </Table.Row>
+        </Table.Body>
+    </Table>
+);
+};
+      `,
+    snippet: (
+      <Table>
+        <Table.Head>
+          <Table.Row>
+            <Table.Cell isHeader>Header 1</Table.Cell>
+            <Table.Cell isHeader>Header 2</Table.Cell>
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>Row 1, Cell 1</Table.Cell>
+            <Table.Cell>Row 1, Cell 2</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Row 2, Cell 1</Table.Cell>
+            <Table.Cell>Row 2, Cell 2</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    ),
+  },
+];
+
+export const columns = ['Name', 'Type', 'Default', 'Description'];
+
+export const tableRows = [
+  ['children', 'node', 'undefined', 'The content to be rendered inside the table.'],
+  ['className', 'string', 'undefined', 'Additional CSS class for the table.'],
+  ['style', 'object', 'undefined', 'Inline styles to be applied to the table.'],
+  ['ref', 'ref', 'undefined', 'Ref forwarded to the table element.'],
+];
+
+export const tableHeadRows = [
+  ['children', 'node', 'undefined', 'The content to be rendered inside the table head.'],
+  ['className', 'string', 'undefined', 'Additional CSS class for the table head.'],
+  ['style', 'object', 'undefined', 'Inline styles to be applied to the table head.'],
+];
+
+export const tableBodyRows = [
+  ['children', 'node', 'undefined', 'The content to be rendered inside the table body.'],
+  ['className', 'string', 'undefined', 'Additional CSS class for the table body.'],
+  ['style', 'object', 'undefined', 'Inline styles to be applied to the table body.'],
+];
+
+export const tableFooterRows = [
+  ['children', 'node', 'undefined', 'The content to be rendered inside the table footer.'],
+  ['className', 'string', 'undefined', 'Additional CSS class for the table footer.'],
+  ['style', 'object', 'undefined', 'Inline styles to be applied to the table footer.'],
+];
+
+export const tableRowRows = [
+  ['children', 'node', 'undefined', 'The content to be rendered inside the table row.'],
+  ['className', 'string', 'undefined', 'Additional CSS class for the table row.'],
+  ['style', 'object', 'undefined', 'Inline styles to be applied to the table row.'],
+];
+
+export const tableCellRows = [
+  ['children', 'node', 'undefined', 'The content to be rendered inside the table cell.'],
+  ['className', 'string', 'undefined', 'Additional CSS class for the table cell.'],
+  ['style', 'object', 'undefined', 'Inline styles to be applied to the table cell.'],
+  [
+    'isHeader',
+    'bool',
+    'false',
+    'Determines if the cell should be rendered as a header cell (th) or a regular cell (td).',
+  ],
+];
