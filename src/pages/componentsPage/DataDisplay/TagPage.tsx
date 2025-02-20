8fb2405a8f498582b6a750a46b71b34cba6f02fe
RaviTeja884781 <ladi.ravi884781@gmail.com>
2025-02-20 11:59:18 +0530

    feat: add TagPage component with documentation and structured data display

--- src/pages/componentsPage/DataDisplay/TagPage.tsx
+++ src/pages/componentsPage/DataDisplay/TagPage.tsx
@@ -1,5 +1,66 @@
+import { SectionHeader } from '@/components/common/SectionHeader/SectionHeader';
+import Separator from '@/components/common/Separator';
+import TableList from '@/components/common/Table/TableList';
+import {
+  docs,
+  columns,
+  tagProps,
+  tagIconProps,
+  tagTextProps,
+  tagCloseButtonProps,
+  additionalNotes,
+} from './data/tagDocs';
+import DocumentationRenderer from '@/components/shared/DocumentationRenderer/DocumentationRenderer';
+import Div from '@/components/common/Div';
+import { Heading } from '@/components/common/Typography';
+import UnOrderedList from '@/components/common/Lists/UnOrderedList';
+
 const TagPage = () => {
-  return <div>TagPage</div>;
+  return (
+    <div className="container">
+      <SectionHeader variant="transparent" size="sm">
+        <SectionHeader.Title className="tracking-wide">Tag</SectionHeader.Title>
+        <SectionHeader.SubTitle>
+          {additionalNotes.map((note, index) => (
+            <UnOrderedList key={index}>
+              <UnOrderedList.Item className="text-muted-foreground tracking-wide">{note}</UnOrderedList.Item>
+            </UnOrderedList>
+          ))}
+        </SectionHeader.SubTitle>
+      </SectionHeader>
+      <Separator />
+      <DocumentationRenderer className="my-4" data={docs} />
+      <SectionHeader variant="transparent" size="sm">
+        <SectionHeader.Title className="tracking-wide">Props</SectionHeader.Title>
+      </SectionHeader>
+      <Div className="space-y-5">
+        <Div>
+          <Heading size="h6" className="mb-2 text-muted-foreground">
+            Tag
+          </Heading>
+          <TableList columns={columns} rows={tagProps} code CodeColumn={0} />
+        </Div>
+        <Div>
+          <Heading size="h6" className="mb-2 text-muted-foreground">
+            Tag Icon
+          </Heading>
+          <TableList columns={columns} rows={tagIconProps} code CodeColumn={0} />
+        </Div>
+        <Div>
+          <Heading size="h6" className="mb-2 text-muted-foreground">
+            Tag Text
+          </Heading>
+          <TableList columns={columns} rows={tagTextProps} code CodeColumn={0} />
+        </Div>
+        <Div>
+          <Heading size="h6" className="mb-2 text-muted-foreground">
+            Tag Close Button
+          </Heading>
+          <TableList columns={columns} rows={tagCloseButtonProps} code CodeColumn={0} />
+        </Div>
+      </Div>
+    </div>
+  );
 };
 
 export default TagPage;
---
+++ src/pages/componentsPage/DataDisplay/data/tagDocs.tsx
@@ -0,0 +1,122 @@
+import Box from '@/components/common/Box';
+import Tag from '@/components/common/Tag';
+import { docsData } from '@/types/docsData';
+import { GlassWaterIcon } from 'lucide-react';
+
+export const docs: docsData[] = [
+  {
+    title: 'Basic Tag',
+    desc: 'A simple tag with text content.',
+    code: `
+import React from 'react';
+import { Tag } from "react-ui-essentials";
+
+const BasicTagExample = () => (
+  <Tag>Basic Tag</Tag>
+);
+      `,
+    snippet: <Tag>Basic Tag</Tag>,
+  },
+  {
+    title: 'Tag with Custom Variant and Size',
+    desc: 'A tag with a custom variant and size.',
+    code: `
+import React from 'react';
+import { Tag } from "react-ui-essentials";
+
+const CustomTagExample = () => (
+  <Tag variant="success" size="lg">Success</Tag>
+);
+      `,
+    snippet: (
+      <Tag variant="success" size="lg">
+        Success
+      </Tag>
+    ),
+  },
+  {
+    title: 'Tag with Icon',
+    desc: 'A tag with an icon and text.',
+    code: `
+import React from 'react';
+import { Tag } from "react-ui-essentials";
+import { RoundedIcons } from "react-ui-essentials-icons";
+const { Dataset } = RoundedIcons;
+
+const IconTagExample = () => (
+  <Tag>
+    <Tag.Icon><Dataset /></Tag.Icon>
+    <Tag.Text>Favorite</Tag.Text>
+  </Tag>
+);
+      `,
+    snippet: (
+      <Tag>
+        <Tag.Icon>
+          <GlassWaterIcon className="size-3" />
+        </Tag.Icon>
+        <Tag.Text>Favorite</Tag.Text>
+      </Tag>
+    ),
+  },
+
+  {
+    title: 'Multiple Tags',
+    desc: 'A group of tags with different variants.',
+    code: `
+import React from 'react';
+import { Tag } from "react-ui-essentials";
+
+const MultipleTagsExample = () => (
+  <div>
+    <Tag variant="primary">Primary</Tag>
+    <Tag variant="secondary">Secondary</Tag>
+    <Tag variant="success">Success</Tag>
+    <Tag variant="danger">Danger</Tag>
+    <Tag variant="warning">Warning</Tag>
+    <Tag variant="info">Info</Tag>
+  </div>
+);
+      `,
+    snippet: (
+      <Box style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
+        <Tag variant="primary">Primary</Tag>
+        <Tag variant="secondary">Secondary</Tag>
+        <Tag variant="success">Success</Tag>
+        <Tag variant="danger">Danger</Tag>
+        <Tag variant="warning">Warning</Tag>
+        <Tag variant="info">Info</Tag>
+      </Box>
+    ),
+  },
+];
+
+export const columns = ['Name', 'Type', 'Default', 'Description'];
+
+export const tagProps = [
+  ['children', 'node', '-', 'The content of the tag. Required prop.'],
+  [
+    'variant',
+    'string',
+    'primary',
+    "Specifies the visual variant of the tag. Possible values are 'primary', 'secondary', 'success', 'warning', 'danger', 'info', 'light', and 'dark'.",
+  ],
+  ['size', 'string', 'sm', "Specifies the size of the tag. Possible values are 'sm', 'md', 'lg', 'xl', and 'xxl'."],
+];
+
+export const tagIconProps = [['children', 'node', '-', 'The icon to be displayed in the tag. Required prop.']];
+
+export const tagTextProps = [['children', 'node', '-', 'The text content of the tag. Required prop.']];
+
+export const tagCloseButtonProps = [
+  ['onClick', 'function', '-', 'The function to be called when the close button is clicked. Required prop.'],
+];
+
+export const additionalNotes = [
+  'The Tag component is composed of several sub-components: Tag.Icon, Tag.Text, and Tag.CloseButton.',
+  'You can use these sub-components to create more complex tag structures.',
+  'The Tag component forwards refs, allowing you to access the underlying DOM element if needed.',
+  'The CloseButton sub-component renders a times symbol (×) by default, but you can override this by passing children to it.',
+  'The Tag component applies CSS classes based on the variant and size props, allowing for easy styling customization.',
+  'When using the CloseButton, remember to handle the visibility state of the tag in your parent component.',
+];
