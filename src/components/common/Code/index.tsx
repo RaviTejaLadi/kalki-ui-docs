3ab05f8c5a67cfe344ed2c18130509835373b0b3
RaviTeja884781 <ladi.ravi884781@gmail.com>
2025-02-21 15:36:44 +0530

    refactor: consolidate Code component and remove unused codeVariants and types files

--- src/components/common/Code/index.tsx
+++ src/components/common/Code/index.tsx
@@ -1,8 +1,62 @@
 import { cn } from '@/utils';
-import { forwardRef } from 'react';
-import { codeVariants } from './codeVariants';
-import type { CodeProps } from './types';
+import React, { forwardRef, ReactNode } from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
 
+// #region codeVariants
+const codeVariants = cva('font-mono inline-flex items-center rounded-md font-semibold ring-1 ring-inset', {
+  variants: {
+    variant: {
+      primary: 'bg-blue-50 text-blue-700 ring-blue-700/10',
+      secondary: 'bg-gray-50 text-gray-600 ring-gray-500/10',
+      success: 'bg-green-50 text-green-700 ring-green-600/20',
+      danger: 'bg-red-50 text-red-700 ring-red-600/10',
+      warning: 'bg-yellow-50 text-yellow-800 ring-yellow-600/20',
+      info: 'bg-indigo-50 text-indigo-700 ring-indigo-700/10',
+      help: 'bg-purple-50 text-purple-700 ring-purple-700/10',
+      light: 'text-foreground bg-background',
+      dark: 'text-gray-800 bg-gray-300',
+    },
+    size: {
+      sm: 'text-[.7rem] py-[.5px] px-2',
+      md: 'text-[.8rem] py-[1px] px-2',
+      lg: 'text-[.9rem] py-[1.5px] px-2',
+    },
+    underline: {
+      true: 'underline',
+      false: '',
+    },
+  },
+  defaultVariants: {
+    variant: 'primary',
+    size: 'sm',
+  },
+});
+// #endregion
+
+// #region types
+/**
+ * Props for the Code component.
+ *
+ * @extends VariantProps<typeof codeVariants>
+ *
+ * @property {ReactNode} children - The content to be rendered inside the code block.
+ * @property {string} [className] - Optional additional class names to apply to the code block.
+ * @property {React.CSSProperties} [style] - Optional inline styles to apply to the code block.
+ * @property {boolean} [underline] - Optional flag to underline the code text.
+ * @property {string} [padding] - Optional padding to apply to the code block.
+ * @property {string} [margin] - Optional margin to apply to the code block.
+ */
+interface CodeProps extends VariantProps<typeof codeVariants> {
+  children: ReactNode;
+  className?: string;
+  style?: React.CSSProperties;
+  underline?: boolean;
+  padding?: string;
+  margin?: string;
+}
+// #endregion
+
+// #region Code
 /**
  * `Code` is a React functional component that renders a `<code>` element with customizable styles and class names.
  * It supports forwarding refs and accepts various props to control its appearance.
@@ -42,6 +96,10 @@ const Code = forwardRef<HTMLElement, CodeProps>(
 );
 
 Code.displayName = 'Code';
+// #endregion
 
+// #region exports
 export default Code;
 export { codeVariants };
+export type { CodeProps };
+// #endregion
