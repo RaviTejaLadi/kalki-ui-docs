951e88e4d6bec2aaface34a8fc992d18ce774c70
RaviTeja884781 <ladi.ravi884781@gmail.com>
2025-02-28 19:25:23 +0530

    refactor: update Stepper component styling for consistency and improved readability

--- src/components/common/Stepper/Stepper.tsx
+++ src/components/common/Stepper/Stepper.tsx
@@ -34,7 +34,7 @@ const StepperContext = createContext({});
 export const Stepper = ({ children, className }: StepperProps) => {
   return (
     <StepperContext.Provider value={{}}>
-      <div className={cn('w-full mx-auto p-6 bg-white text-foreground dark:text-gray-100', className)}>
+      <div className={cn('w-full mx-auto p-6 bg-background text-foreground ', className)}>
         <div className="relative">{children}</div>
       </div>
     </StepperContext.Provider>
@@ -45,13 +45,13 @@ const StepperStep: React.FC<StepProps> = ({ step, title, children }) => {
   return (
     <div className="relative flex items-start gap-8 mb-10 group">
       {step && (
-        <div className="relative flex-shrink-0 w-8 h-8 text-center bg-gray-100 border rounded-full flex items-center justify-center text-sm font-bold dark:bg-gray-700 dark:border-gray-600">
+        <div className="relative flex-shrink-0 w-8 h-8 text-center bg-background border rounded-full flex items-center justify-center text-sm font-bold dark:border-gray-200/10">
           {step}
         </div>
       )}
       <div className="flex-1">
-        <h3 className="text-md text-foreground font-semibold mb-3 dark:text-gray-200">{title}</h3>
-        <div className="text-base dark:text-gray-300">{children}</div>
+        <h3 className="text-md text-foreground font-semibold mb-3">{title}</h3>
+        <div className="text-base text-muted-foreground">{children}</div>
       </div>
     </div>
   );
@@ -59,14 +59,14 @@ const StepperStep: React.FC<StepProps> = ({ step, title, children }) => {
 
 const StepperCodeBlock: React.FC<CodeBlockProps> = ({ code, language, ...rest }) => {
   return (
-    <div className="rounded-md font-mono text-sm text-gray-800 mt-2 dark:text-gray-300">
+    <div className="rounded-md font-mono text-sm text-foreground mt-2 ">
       <SyntaxHighlighter code={code} language={language} {...rest} />
     </div>
   );
 };
 
 const StepperDescription: React.FC<DescriptionProps> = ({ children, className }) => {
-  return <p className={cn('text-muted-foreground mb-4 dark:text-gray-400', className)}>{children}</p>;
+  return <p className={cn('text-muted-foreground mb-4', className)}>{children}</p>;
 };
 
 Stepper.Step = StepperStep;
