c3e87bbd7d23d7b600b6d6f540d1b9bf253deef3
Ravi Teja <ladi.ravi884781@gmail.com>
2025-03-21 17:39:18 +0530

    refactor: update Tag component to use div instead of span and enhance styling for better layout

--- src/components/common/Tag/index.tsx
+++ src/components/common/Tag/index.tsx
@@ -3,7 +3,8 @@ import { cn } from '@/utils';
 import { cva, VariantProps } from 'class-variance-authority';
 import { X } from 'lucide-react';
 
-const tagVariants = cva('inline-flex items-center font-medium text-center rounded-md transition', {
+// #region tag variants
+const tagVariants = cva('inline-flex items-center font-medium text-center transition relative', {
   variants: {
     variant: {
       primary: 'bg-blue-500 text-white hover:bg-blue-600',
@@ -16,10 +17,10 @@ const tagVariants = cva('inline-flex items-center font-medium text-center rounde
       dark: 'bg-gray-800 text-white hover:bg-gray-900',
     },
     size: {
-      xs: 'text-xs h-6 p-1',
-      sm: 'text-sm h-8 p-1.5',
-      md: 'text-base h-10 p-1.5',
-      lg: 'text-lg h-12 p-2',
+      xs: 'text-xs min-h-6 py-1 pl-5 pr-3',
+      sm: 'text-sm min-h-8 py-1.5 pl-7 pr-4',
+      md: 'text-base min-h-10 py-2 pl-8 pr-5',
+      lg: 'text-lg min-h-12 py-2.5 pl-10 pr-6',
     },
   },
   defaultVariants: {
@@ -30,7 +31,7 @@ const tagVariants = cva('inline-flex items-center font-medium text-center rounde
 // #endregion
 
 // #region type
-interface TagProps extends React.HTMLAttributes<HTMLSpanElement>, VariantProps<typeof tagVariants> {
+interface TagProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof tagVariants> {
   children: React.ReactNode;
 }
 
@@ -43,6 +44,7 @@ interface TagTextProps extends React.HTMLAttributes<HTMLSpanElement> {
   children: React.ReactNode;
   className?: string;
 }
+
 interface TagCloseButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   onClick: () => void;
   className?: string;
@@ -50,30 +52,47 @@ interface TagCloseButtonProps extends React.ButtonHTMLAttributes<HTMLButtonEleme
 // #endregion
 
 // #region Tag
-const Tag = forwardRef<HTMLSpanElement, TagProps>(({ children, variant, size, className, ...rest }, ref) => {
+const Tag = forwardRef<HTMLDivElement, TagProps>(({ children, variant, size, className, ...rest }, ref) => {
   return (
-    <span ref={ref} className={cn(tagVariants({ variant, size }), className)} {...rest}>
-      {children}
-    </span>
+    <div
+      ref={ref}
+      className={cn(
+        tagVariants({ variant, size }),
+        'clip-path-tag',
+        'before:content-[""] before:absolute before:left-3 before:top-1/2 before:-translate-y-1/2 before:w-2 before:h-2 before:rounded-full before:bg-gray-300 before:border-[1px] before:border-gray-200/10  before:opacity-70',
+        'before:ring-1 before:ring-current before:ring-opacity-30',
+        className
+      )}
+      style={{
+        clipPath: 'polygon(0% 0%, 88% 0%, 100% 50%, 88% 100%, 0% 100%)',
+      }}
+      {...rest}
+    >
+      <div className="flex items-center z-10 relative">{children}</div>
+    </div>
   );
 });
 
 Tag.displayName = 'Tag';
 
 const TagIcon: React.FC<TagIconProps> = ({ children, className, ...rest }) => (
-  <span className={cn('inline-flex items-center mx-1', className)} {...rest}>
+  <span className={cn('inline-flex items-center mr-1', className)} {...rest}>
     {children}
   </span>
 );
 
 const TagClose: React.FC<TagCloseButtonProps> = ({ onClick, className, ...props }) => (
-  <button className={cn('ml-2 p-1 opacity-50 hover:opacity-100 transition', className)} onClick={onClick} {...props}>
+  <button
+    className={cn('ml-1 p-1 opacity-70 hover:opacity-100 transition z-10', className)}
+    onClick={onClick}
+    {...props}
+  >
     <X className="size-3 font-bold" />
   </button>
 );
 
 const TagText: React.FC<TagTextProps> = ({ children, className, ...rest }) => (
-  <span className={cn('text-center', className)} {...rest}>
+  <span className={cn('text-center z-10', className)} {...rest}>
     {children}
   </span>
 );
