95d0bbdfd363b3639e3d993a7405d2197e22a015
Ravi Teja <ladi.ravi884781@gmail.com>
2025-04-11 14:14:00 +0530

    feat: Add SEO metadata to BadgePage and update header rendering

--- src/pages/componentsPage/DataDisplay/BadgePage.tsx
+++ src/pages/componentsPage/DataDisplay/BadgePage.tsx
@@ -3,15 +3,21 @@ import Separator from '@/components/common/Separator';
 import TableList from '@/components/common/Table/TableList';
 import { docs, columns, rows } from './data/badgeDocs';
 import DocumentationRenderer from '@/components/shared/DocumentationRenderer/DocumentationRenderer';
+import { IMetaData } from '@/types/metaData';
+import Head from '@/components/Head';
+
+const metaData: IMetaData = {
+  title: 'Badge',
+  description: `Badge is a small status indicator for another element.`,
+};
 
 const BadgePage = () => {
   return (
     <div className="container">
+      <Head {...metaData} />
       <SectionHeader variant="transparent" size="sm">
-        <SectionHeader.Title className="tracking-wide">Badge</SectionHeader.Title>
-        <SectionHeader.SubTitle className="tracking-wide">
-          Badge is a small status indicator for another element.
-        </SectionHeader.SubTitle>
+        <SectionHeader.Title className="tracking-wide">{metaData.title}</SectionHeader.Title>
+        <SectionHeader.SubTitle className="tracking-wide">{metaData.description}</SectionHeader.SubTitle>
       </SectionHeader>
       <Separator />
       <DocumentationRenderer className="my-4" data={docs} />
