6f4c9c7d5380ed093a1b8e7c8599d06f7e37e89c
Ravi Teja <ladi.ravi884781@gmail.com>
2025-04-11 14:28:30 +0530

    feat: Add SEO metadata to ModelPage and update header rendering

--- src/pages/componentsPage/Feedback/ModelPage.tsx
+++ src/pages/componentsPage/Feedback/ModelPage.tsx
@@ -13,12 +13,20 @@ import {
 import DocumentationRenderer from '@/components/shared/DocumentationRenderer/DocumentationRenderer';
 import { Heading } from '@/components/common/Typography';
 import Div from '@/components/common/Div';
+import Head from '@/components/Head';
+import { IMetaData } from '@/types/metaData';
 
+const metaData: IMetaData = {
+  title: 'Modal',
+  description: `Modal component to inform users about a task or important information that requires decisions, or involves multiple tasks.`,
+};
 const ModelPage = () => {
   return (
     <div className="container">
+      <Head {...metaData} />
       <SectionHeader variant="transparent" size="sm">
-        <SectionHeader.Title className="tracking-wide">Modal</SectionHeader.Title>
+        <SectionHeader.Title className="tracking-wide">{metaData.title}</SectionHeader.Title>
+        <SectionHeader.SubTitle className="tracking-wide">{metaData.description}</SectionHeader.SubTitle>
       </SectionHeader>
       <Separator />
       <DocumentationRenderer className="my-4" data={docs} />
