dfe244c8e7b7468b5150561e88e767457b9ab54a
Ravi Teja <ladi.ravi884781@gmail.com>
2025-04-11 14:17:35 +0530

    feat: Add SEO metadata to OrderedListPage and update header rendering

--- src/pages/componentsPage/DataDisplay/OrderedListPage.tsx
+++ src/pages/componentsPage/DataDisplay/OrderedListPage.tsx
@@ -5,13 +5,21 @@ import { columns, docs, orderedListRows, listItemRows } from './data/OrderedList
 import Div from '@/components/common/Div';
 import { Heading } from '@/components/common/Typography';
 import TableList from '@/components/common/Table/TableList';
+import { IMetaData } from '@/types/metaData';
+import Head from '@/components/Head';
+
+const metaData: IMetaData = {
+  title: 'Ordered List',
+  description: `Used to display a list of items`,
+};
 
 const OrderedListPage = () => {
   return (
     <div className="container">
+      <Head {...metaData} />
       <SectionHeader variant="transparent" size="sm">
-        <SectionHeader.Title className="tracking-wide">Ordered List</SectionHeader.Title>
-        <SectionHeader.SubTitle className="tracking-wide">Used to display a list of items</SectionHeader.SubTitle>
+        <SectionHeader.Title className="tracking-wide">{metaData.title}</SectionHeader.Title>
+        <SectionHeader.SubTitle className="tracking-wide">{metaData.description}</SectionHeader.SubTitle>
       </SectionHeader>
       <Separator />
       <DocumentationRenderer data={docs} />
