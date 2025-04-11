61af1219d2fdd84c2d2a656462895c64adaa9ad0
Ravi Teja <ladi.ravi884781@gmail.com>
2025-04-11 14:35:50 +0530

    feat: Add SEO metadata to BoxPage and update header rendering

--- src/pages/componentsPage/LayoutPage/BoxPage.tsx
+++ src/pages/componentsPage/LayoutPage/BoxPage.tsx
@@ -4,12 +4,23 @@ import Separator from '@/components/common/Separator';
 import TableList from '@/components/common/Table/TableList';
 import DocumentationRenderer from '@/components/shared/DocumentationRenderer/DocumentationRenderer';
 import { docs, columns, rows } from './data/boxData';
+import { IMetaData } from '@/types/metaData';
+import Head from '@/components/Head';
+
+const metaData: IMetaData = {
+  title: 'Box',
+  description: `Box component is a versatile container that provides several customization
+          options, including size ,padding ,margin ,elevation (box-shadow) ,border radius,border styles, and
+          colors. It can be used to create consistent and styled containers across your
+          application.`,
+};
 
 const BoxPage = () => {
   return (
     <div className="container">
+      <Head {...metaData} />
       <SectionHeader variant="transparent" size="sm">
-        <SectionHeader.Title className="tracking-wide">Box</SectionHeader.Title>
+        <SectionHeader.Title className="tracking-wide">{metaData.title}</SectionHeader.Title>
         <SectionHeader.SubTitle className="tracking-wide">
           The <Code variant="light">Box</Code> component is a versatile container that provides several customization
           options, including <Code variant="light">size</Code> ,<Code variant="light">padding</Code>,
