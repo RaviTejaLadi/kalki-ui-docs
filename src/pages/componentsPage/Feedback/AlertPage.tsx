c15fe2b4056744cb038d01fc30328b5dd4417d3f
Ravi Teja <ladi.ravi884781@gmail.com>
2025-04-11 14:25:44 +0530

    feat: Add SEO metadata to AlertPage and update header rendering

--- src/pages/componentsPage/Feedback/AlertPage.tsx
+++ src/pages/componentsPage/Feedback/AlertPage.tsx
@@ -3,16 +3,22 @@ import Separator from '@/components/common/Separator';
 import TableList from '@/components/common/Table/TableList';
 import { docs, columns, rows } from './data/alertData';
 import DocumentationRenderer from '@/components/shared/DocumentationRenderer/DocumentationRenderer';
+import { IMetaData } from '@/types/metaData';
+import Head from '@/components/Head';
+
+const metaData: IMetaData = {
+  title: 'Alerts',
+  description: `Alerts are used to capture the user's attention for various reasons such as informing them of important
+          events, errors, warnings, or other significant information.`,
+};
 
 const AlertPage = () => {
   return (
     <div className="container">
+      <Head {...metaData} />
       <SectionHeader variant="transparent" size="sm">
-        <SectionHeader.Title className="tracking-wide">Alerts</SectionHeader.Title>
-        <SectionHeader.SubTitle className="tracking-wide">
-          Alerts are used to capture the user&apos;s attention for various reasons such as informing them of important
-          events, errors, warnings, or other significant information.
-        </SectionHeader.SubTitle>
+        <SectionHeader.Title className="tracking-wide">{metaData.title}</SectionHeader.Title>
+        <SectionHeader.SubTitle className="tracking-wide">{metaData.description}</SectionHeader.SubTitle>
       </SectionHeader>
       <Separator />
       <DocumentationRenderer className="my-4" data={docs} />
