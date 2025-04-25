66ce66e66b134217940efb5da4f7f66cb952e969
Ravi Teja <ladi.ravi884781@gmail.com>
2025-04-25 13:32:36 +0530

    feat: Integrate toast notifications in CheckBoxGroup example for form submission feedback

--- src/pages/componentsPage/FormsPage/examples/CheckBoxGroupEg.tsx
+++ src/pages/componentsPage/FormsPage/examples/CheckBoxGroupEg.tsx
@@ -1,5 +1,7 @@
 import Form, { Checkbox, FormGroup, Submit } from '@/components/common/Form';
 import JsonViewer from '@/components/common/JsonViewer';
+import Separator from '@/components/common/Separator';
+import { useToast } from 'kalki-ui-toast';
 import { FormEvent, useState } from 'react';
 
 type FormState = {
@@ -7,6 +9,7 @@ type FormState = {
 };
 
 export const CheckBoxGroupEg = () => {
+  const { addToast } = useToast();
   const [formState, setFormState] = useState<FormState>({
     folders: [],
   });
@@ -26,11 +29,20 @@ export const CheckBoxGroupEg = () => {
       return { ...prev, folders };
     });
   };
+  
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    console.log(formState);
+    addToast({
+      message: (
+        <div className="p-2">
+          <p className="text-muted-foreground">Form Data:</p>
+          <Separator className="h-[0.5px] mt-2 bg-gray-200" />
+          <JsonViewer data={formState} />
+        </div>
+      ),
+    });
   };
+
   return (
     <div className="flex gap-5 ">
       <Form onSubmit={handleSubmit}>
