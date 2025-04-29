bfb284212050dede4c3f54656e148d97f366d90f
Ravi Teja <ladi.ravi884781@gmail.com>
2025-04-29 10:34:29 +0530

    refactor: Remove Badge component files and consolidate Badge and BadgeIcon into index.tsx with updated types

--- src/components/common/Badge/index.tsx
+++ src/components/common/Badge/index.tsx
@@ -1,25 +1,56 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, HTMLAttributes } from 'react';
 import { cn } from '@/utils';
-import { badgeVariants } from './badgeVariants';
-import type { BadgeProps, BadgeIconProps } from './types';
-import { BadgeIcon } from './BadgeIcon';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-/**
- * `Badge` is a React functional component that renders a styled `<span>` element.
- * It uses `forwardRef` to pass down a ref to the `<span>` element.
- *
- * @param {BadgeProps} props - The properties passed to the component.
- * @param {string} props.size - The size of the badge.
- * @param {string} props.variant - The variant of the badge.
- * @param {boolean} props.pill - If true, the badge will have a pill shape.
- * @param {string} [props.className] - Additional class names to apply to the badge.
- * @param {React.CSSProperties} [props.style] - Inline styles to apply to the badge.
- * @param {React.ReactNode} props.children - The content to be displayed inside the badge.
- * @param {React.Ref<HTMLDivElement>} ref - The ref to be forwarded to the `<span>` element.
- * @param {object} rest - Any additional props to be passed to the `<span>` element.
- *
- * @returns {JSX.Element} The rendered badge component.
- */
+// #region badgeVariants
+const badgeVariants = cva(
+  'inline-flex gap-2 m-1 transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 w-fit items-center font-bold text-center align-middle',
+  {
+    variants: {
+      size: {
+        sm: 'text-xs py-1 px-2 rounded-sm',
+        md: 'text-sm py-1.5 px-3 rounded-md',
+        lg: 'text-base py-2 px-4 rounded-lg',
+      },
+      variant: {
+        primary: 'bg-primary text-white',
+        secondary: 'bg-secondary text-white',
+        success: 'bg-success text-white',
+        danger: 'bg-danger text-white',
+        warning: 'bg-warning text-black',
+        info: 'bg-info text-white',
+        help: 'bg-help text-white',
+        light: 'bg-light text-foreground',
+        dark: 'bg-dark text-white',
+        outline: 'border border-gray-200/10 text-foreground',
+      },
+      pill: {
+        true: 'rounded-full',
+        false: '',
+      },
+    },
+    defaultVariants: {
+      size: 'sm',
+      variant: 'primary',
+      pill: false,
+    },
+  }
+);
+
+// #endregion
+
+// #region types
+
+interface BadgeProps extends HTMLAttributes<HTMLSpanElement>, VariantProps<typeof badgeVariants> {
+  children: React.ReactNode;
+}
+
+interface BadgeIconProps extends React.HTMLAttributes<HTMLSpanElement> {
+  className?: string;
+}
+// #endregion
+
+// #region Badge
 const Badge = forwardRef<HTMLDivElement, BadgeProps>(
   ({ size, variant, pill, className, style, children, ...rest }, ref) => {
     return (
@@ -32,6 +63,15 @@ const Badge = forwardRef<HTMLDivElement, BadgeProps>(
 
 Badge.displayName = 'Badge';
 
+const BadgeIcon = forwardRef<HTMLSpanElement, BadgeIconProps>(({ className, ...rest }, ref) => (
+  <span ref={ref} className={cn(className)} {...rest} />
+));
+
+BadgeIcon.displayName = 'BadgeIcon';
+
+// #endregion
+
+// #region exports
 export default Object.assign(
   Badge as React.ForwardRefExoticComponent<BadgeProps & React.RefAttributes<HTMLDivElement>>,
   {
@@ -39,4 +79,6 @@ export default Object.assign(
   }
 );
 
-export { BadgeIcon, BadgeProps, BadgeIconProps };
+export { BadgeIcon, badgeVariants };
+export type { BadgeProps, BadgeIconProps };
+// #endregion
