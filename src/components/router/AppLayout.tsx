02869c8c59fa7c87470d07d3b516bf205016847c
Ravi Teja <ladi.ravi884781@gmail.com>
2025-05-23 16:41:28 +0530

    feat: integrate Radix UI components for improved navigation and layout
    - Added Radix UI Accordion, Dialog, and Navigation Menu components to enhance UI functionality.
    - Replaced TopBar with a new Navbar component that utilizes the Navigation Menu for better mobile and desktop navigation.
    - Implemented a Sheet component for mobile menu interactions.
    - Updated package.json to include new Radix UI dependencies.
    - Refactored AppLayout to accommodate the new Navbar structure.

--- src/components/router/AppLayout.tsx
+++ src/components/router/AppLayout.tsx
@@ -1,14 +1,15 @@
 import { Suspense } from 'react';
-import TopBar from '../shared/TopBar/TopBar';
+// import TopBar from '../shared/TopBar/TopBar';
 import { Outlet } from 'react-router-dom';
 import ScrollToTop from '../common/ScrollToTop';
 import Loading from '../shared/Loading';
+import { Navbar } from '../shared/TopBar/NavBar';
 
 export const AppLayout = () => {
   return (
     <div className="relative min-h-screen flex flex-col ">
       <div className="fixed top-0 left-0 right-0 z-50 h-11">
-        <TopBar />
+        <Navbar />
       </div>
       <main className="flex-grow pt-11 ">
         <Suspense fallback={<Loading />}>
---
+++ src/components/shared/TopBar/NavBar.tsx
@@ -0,0 +1,178 @@
+import Button from '@/components/common/Button';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/schadcn/accordion';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from '@/components/schadcn/navigation-menu';
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/schadcn/sheet';
+import { Github, Menu } from 'lucide-react';
+import React from 'react';
+import Logo from './Logo';
+import Div from '@/components/common/Div';
+import { ThemeToggle } from './ThemeToggle';
+
+interface MenuItem {
+  title: string;
+  url: string;
+  description?: string;
+  icon?: React.ReactNode;
+  items?: MenuItem[];
+}
+
+interface Navbar1Props {
+  menu?: MenuItem[];
+}
+
+const Navbar = ({
+  menu = [
+    { title: 'Get Started', url: '/get-started' },
+    { title: 'Components', url: '/components' },
+    { title: 'Blocks', url: '/blocks' },
+    { title: 'State Vault', url: 'https://state-vault-docs.vercel.app/' },
+    { title: 'Colors', url: '/colors' },
+  ],
+}: Navbar1Props) => {
+  return (
+    <div className="flex h-11 px-10 justify-between space-x-10 fixed top-0 z-50 w-full  border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-inherit">
+      {/* Desktop Menu */}
+      <nav className="hidden justify-between lg:flex w-full">
+        <div className="flex items-center justify-between gap-6 w-full">
+          {/* Logo */}
+          <Div className="flex items-center ">
+            <a href="/" className="text-md w-auto">
+              <Logo />
+            </a>
+            <div className="flex items-center">
+              <NavigationMenu>
+                <NavigationMenuList className="text-muted-foreground">
+                  {menu.map((item) => renderMenuItem(item))}
+                </NavigationMenuList>
+              </NavigationMenu>
+            </div>
+          </Div>
+          <Div className="hidden md:block">
+            <ThemeToggle />
+            <a href="https://github.com/RaviTejaLadi/kalki-ui" target="_blank" rel="noopener noreferrer">
+              <Button variant="outline" size="xs">
+                <Github className="h-4 w-4" />
+              </Button>
+            </a>
+          </Div>
+        </div>
+      </nav>
+
+      {/* Mobile Menu */}
+      <div className="block lg:hidden py-2 items-center w-full">
+        <div className="flex items-center justify-between">
+          {/* Logo */}
+          <a href="/" className="text-md w-auto">
+            <Logo />
+          </a>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="xs">
+                <Menu className="size-4" />
+              </Button>
+            </SheetTrigger>
+            <SheetContent className="overflow-y-auto">
+              <SheetHeader>
+                <SheetTitle>
+                  <a href="/" className="text-md w-auto">
+                    <Logo />
+                  </a>
+                </SheetTitle>
+              </SheetHeader>
+              <div className="flex flex-col mt-4 gap-6 p-4">
+                <Accordion type="single" collapsible className="flex w-full flex-col gap-4">
+                  {menu.map((item) => renderMobileMenuItem(item))}
+                </Accordion>
+              </div>
+              <Div className=" flex items-center justify-end">
+                <ThemeToggle />
+                <a href="https://github.com/RaviTejaLadi/kalki-ui" target="_blank" rel="noopener noreferrer">
+                  <Button variant="outline" size="xs">
+                    <Github className="h-4 w-4" />
+                  </Button>
+                </a>
+              </Div>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const renderMenuItem = (item: MenuItem) => {
+  if (item.items) {
+    return (
+      <NavigationMenuItem key={item.title}>
+        <NavigationMenuTrigger>{item.title}</NavigationMenuTrigger>
+        <NavigationMenuContent className="bg-popover text-popover-foreground">
+          {item.items.map((subItem) => (
+            <NavigationMenuLink asChild key={subItem.title} className="w-80">
+              <SubMenuLink item={subItem} />
+            </NavigationMenuLink>
+          ))}
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+    );
+  }
+
+  return (
+    <NavigationMenuItem key={item.title}>
+      <NavigationMenuLink
+        href={item.url}
+        className="group inline-flex h-6 w-max items-center justify-center rounded bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-muted hover:text-accent-foreground"
+      >
+        {item.title}
+      </NavigationMenuLink>
+    </NavigationMenuItem>
+  );
+};
+
+const renderMobileMenuItem = (item: MenuItem) => {
+  if (item.items) {
+    return (
+      <AccordionItem key={item.title} value={item.title} className="border-b-0">
+        <AccordionTrigger className="text-md py-0 font-semibold hover:no-underline">{item.title}</AccordionTrigger>
+        <AccordionContent className="mt-2">
+          {item.items.map((subItem) => (
+            <SubMenuLink key={subItem.title} item={subItem} />
+          ))}
+        </AccordionContent>
+      </AccordionItem>
+    );
+  }
+
+  return (
+    <a
+      key={item.title}
+      href={item.url}
+      className="group flex justify-start h-6 w-full text-muted-foreground items-center  rounded bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-muted hover:text-accent-foreground"
+    >
+      {item.title}
+    </a>
+  );
+};
+
+const SubMenuLink = ({ item }: { item: MenuItem }) => {
+  return (
+    <a
+      className="flex flex-row gap-4 rounded-md p-3 leading-none no-underline transition-colors outline-none select-none hover:bg-muted hover:text-accent-foreground"
+      href={item.url}
+    >
+      <div className="text-foreground">{item.icon}</div>
+      <div>
+        <div className="text-sm font-semibold">{item.title}</div>
+        {item.description && <p className="text-sm leading-snug text-muted-foreground">{item.description}</p>}
+      </div>
+    </a>
+  );
+};
+
+export { Navbar };
